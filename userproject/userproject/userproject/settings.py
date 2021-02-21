d4846582521bbe6ba900908cf8d510152feaf975
Rahulsaraf1578 <rahulsaraf1578@gmail.com>
2021-02-21 14:57:30 +0530

    changes in new project

--- userproject/userproject/userproject/settings.py
+++ userproject/userproject/userproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!^^4f23a%+d3!tka%zvcb85=on8t@tq+w_@(37vq83&nxdd@x)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
